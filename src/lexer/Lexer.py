cee95e1cdda19ffbe95d3a883de4d12f4bcecebe
nikhilhiremath65 <nikhilhiremath65@gmail.com>
2021-04-02 12:49:10 -0700

    Lexer: Ingnoring singleline comments and creating tokens for conditional statements

--- src/lexer/Lexer.py
+++ src/lexer/Lexer.py
@@ -4,34 +4,46 @@ from sly import Lexer
 
 class CalcLexer(Lexer):
     # Set of token names.   This is always required
-    tokens = {ID, NUMBER, PLUS, MINUS, TIMES,
-              DIVIDE, ASSIGN, LPAREN, RPAREN}
+    tokens = {ID, EQUAL, LE, LT, GE, GT, ASSIGN, NUMBER, PLUS, MINUS, MULTIPLY,
+              DIVIDE, MODULO, POW, LPAREN, RPAREN}
 
     # String containing ignored characters between tokens
     ignore = ' \t'
+    ignore_newline = r'\n+'
+    ignore_comment = r'\#(.*)'
 
     # Regular expression rules for tokens
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    EQUAL = r'=='
+    LE = r'<='
+    GE = r'>='
+
+    LT = r'<'
+    GT = r'>'
+    ASSIGN = r'='
+
     NUMBER = r'\d+'
     PLUS = r'\+'
     MINUS = r'-'
-    TIMES = r'\*'
+    MULTIPLY = r'\*'
     DIVIDE = r'/'
-    ASSIGN = r'='
+    MODULO = r'%'
+    POW = r'\^'
+
     LPAREN = r'\('
     RPAREN = r'\)'
 
 
 def read_program():
-    # Checking file extension
+    # Checking the file extension
     file_extn = sys.argv[1][-4:]
-    # print(file_extn)
     if file_extn == ".imp":
         # Handling file operations and file errors
         try:
             with open(sys.argv[1], "r") as input_file:
                 program = input_file.read()
-                print("program " + program)
+                print("program: " + program)
         except FileNotFoundError:
             print("No such file in path:", sys.argv[1])
         return program
@@ -43,5 +55,7 @@ def read_program():
 if __name__ == '__main__':
     data = read_program()
     lexer = CalcLexer()
-    for tok in lexer.tokenize(data):
-        print(tok.value)
+    output_file = "tokens.txt"
+    with open(output_file, "w") as f:
+        for token in lexer.tokenize(data):
+            f.write('{}\n'.format(token.value))
