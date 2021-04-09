88e0f2a9be483900036fd7ffefb421f5368aa649
kamalpenmetcha <kamalpenmetcha@gmail.com>
2021-04-08 20:31:16 -0700

    User story #16, parsing Print command

--- src/parser/kamalParser.pl
+++ src/parser/kamalParser.pl
@@ -1,8 +1,11 @@
 % PARSER
 
+% Parsing program and block 
 program(t_program(X)) --> block(X), [.].
-
 block(t_block(X)) --> ['{'], command_list(X), ['}'].  
+
+% Parsing print command
+print_command(t_print(X)) --> ['print'], ['('], expression(X), [')'], end_of_command. 
 
 
 
