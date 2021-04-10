2e1ea564db46e96bd6d4cfec9122c77ab72b86f8
Karandeep <kgrewal2@asu.edu>
2021-04-10 11:52:01 -0700

    :white_check_mark: Adding tests for operator and boolean_operators :sparkles: operator and boolean_operator now generating the parse tree

--- src/Parser.pl
+++ src/Parser.pl
@@ -4,8 +4,17 @@ ternary_expression(t_ternary_expression(Condition, TrueExpression, FalseExpressi
 operator --> boolean_operator.
 operator --> [+] | [-] | [*] | [/].
 
-boolean_operator --> and_operator | or_operator | not_operator.
+boolean_operator(B) --> and_operator(B) | or_operator(B) | not_operator(B).
 
-and_operator --> [and].
-or_operator  --> [or].
-not_operator --> [not].
+and_operator(bool_and) --> [and].
+or_operator(bool_or)  --> [or].
+not_operator(bool_not) --> [not].
+
+% TEST CODE
+?- boolean_operator(bool_and, [and, end], [end]).
+?- boolean_operator(bool_or, [or, end], [end]).
+?- boolean_operator(bool_not, [not, end], [end]).
+
+?- and_operator(bool_and, [and, end], [end]).
+?- or_operator(bool_or, [or, end], [end]).
+?- not_operator(bool_not, [not, end], [end]).
