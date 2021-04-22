39cdb6fd57e3286f58effe39b34b071b957704a6
Karandeep <kgrewal2@asu.edu>
2021-04-21 18:22:05 -0700

    Fixing Lexer.py

--- src/Lexer.py
+++ src/Lexer.py
@@ -1,10 +1,12 @@
 import sys
+import argparse
 from sly import Lexer
 
 
 class Lexer(Lexer):
     # SET OF TOKENS
-    tokens = {ASSIGN, DECREMENT, DIVIDE, DOUBLE_QUOTES, EQUAL, FLOAT, GE, GT, ID, INCREMENT, LE, LT, MINUS, MODULO, MULTIPLY, NOT_EQUAL, NUMBER, PLUS, POW, SINGLE_QUOTES, STRING}
+    tokens = {ASSIGN, DECREMENT, DIVIDE, DOUBLE_QUOTES, EQUAL, FLOAT, GE, GT, ID, INCREMENT, LE, LT, MINUS, MODULO,
+              MULTIPLY, NOT_EQUAL, NUMBER, PLUS, POW, SINGLE_QUOTES, STRING}
 
     # LITERALS
     literals = {'{', '}', ',', '?', ';', ':', '[', ']', '(', ')', '.', '!'}
@@ -37,3 +39,38 @@ class Lexer(Lexer):
     POW = r'\^'
     SINGLE_QUOTES = r'\''
     STRING = r'".*"'
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='IMPRO Lexer - Stores the IMPRO source code into a list of tokens as <filename>.impt')
+    parser.add_argument('input', metavar='InputFile', type=str,
+                        nargs=1, help='Input File that contains the IMPRO source code')
+    return parser.parse_args()
+
+
+def read_input_file(filename):
+    data = None
+    try:
+        with open(filename, "r") as input_file:
+            data = input_file.read()
+    except FileNotFoundError:
+        print("No such file in path:", sys.argv[1])
+    return data
+
+
+def write_tokens_to_file(tokens, filename):
+    with open(filename, "w") as file:
+        for token in tokens:
+            file.write('{}\n'.format(token.value))
+
+
+if __name__ == '__main__':
+    parsed_args = parse_arguments()
+    input_filename = parsed_args.input[0]
+    output_filename = parsed_args.input[0]+'tokens'
+    file_data = read_input_file(input_filename)
+
+    lexer = Lexer()
+    print(file_data)
+    tokens = lexer.tokenize(file_data)
+    write_tokens_to_file(tokens, output_filename)
---
+++ src/TokenReader.pl
@@ -0,0 +1,2 @@
+% This helps to read the tokens from filename.imptokens and convert into a list that can be used by the Parser.pl
+
