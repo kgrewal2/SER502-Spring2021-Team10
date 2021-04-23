65d6a0df37c263bcb13d9e2ec4730e3bc76353b3
kamalpenmetcha <kamalpenmetcha@gmail.com>
2021-04-22 22:22:29 -0700

    Tested Environment lookup and update

--- src/Evaluator.pl
+++ src/Evaluator.pl
@@ -8,7 +8,7 @@ update(Id, Val, [(Id,_)|T], [(Id,Val)|T]).
 update(Id, Val, [H|T], [H| Env]):- H \= (Id, _), update(Id, Val, T, Env).
 
 % Evaluating program
-eval_program(t_program(P)) :- eval_command_list(CL, [], _).
+eval_program(t_program(_P)) :- eval_command_list(_CL, [], _).
 
 % Evaluating command list
 eval_command_list(t_command_list(C, CL), Env, EnvRes):- 
@@ -33,3 +33,16 @@ booleanValue(Val, true) :- Val \= false.
 % Evaluating print command
 eval_print_command(t_print(Expression), Env, EnvRes) :-
 	eval_expression(Expression, Env, EnvRes).
+	
+	
+	
+%%%%%%%%%%%	
+% TESTING %
+%%%%%%%%%%%
+
+% ENVIRONMENT LOOKUP AND UPDATE
+?- lookup(x, [(x,3), (y,5)], _X).
+?- lookup(y, [(z,3), (y,5)], _X).
+?- update(x, 4, [], _Res).
+?- update(x, 5, [(x,4), (y,6)], _Res).
+?- update(y, 5, [(x,4), (y,6)], _Res).
